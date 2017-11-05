b91fabdd0098ea1ca5da7fd2ec84c3b2392d4b05
fredspivock <fredspivock@gmail.com>
2017-11-05 15:36:21 -0500

    added header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
+/* Feature Modules */
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: ''},
+      {path: '', redirectTo: '', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent},
+    ], {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
